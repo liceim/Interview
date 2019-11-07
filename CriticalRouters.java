0490b00b3bb26a57bee3e5f32e7fd6bbef188dbc
liceim <libin.im@gmail.com>
2019-11-06 21:27:26 -0800

    Update CriticalRouters.java

--- CriticalRouters.java
+++ CriticalRouters.java
@@ -3,50 +3,54 @@
 //disc[u] records the time when u was discovered
 
 //The Time complexity is O(graph) + O(DFS) = O(|E| + |V|) + O(|E| + |V|) = O(|E| + |V|)
-private int time;
+private List<Integer>[] adj;
+private int id;
+private int[] visited;
+private int[] low;
+
 private static List<Integer> getCriticalNodes(int numRouters, int numLinks, ArrayList<ArrayList<Integer>> linkes) {
-	time = 0;
-	Map<Integer, Set<Integer>> map = new HashMap<>();
+	adj = new List[numRouters + 2];
+	Arrays.setAll(adj, (i) -> new ArrayList<>());
+	for (ArrayList<Integer> l : links) {
+		int u = l.get(0);
+		int v = l.get(0);
+		adj[u].add(v);
+		adj[v].add(u);
+	}
 	
-	for(List<Integer> link : links) {
-		if (!map.containsKey(link.get(0))) {
-			map.put(link.get(0), new HashSet<>());
-		}
-		map.get(link[0]).add(link[1]);
-		
-		if (!map.containsKey(link.get(1))) {
-			map.put(link.get(1), new HashSet<>());
-		}
-		map.get(link[1]).add(link[0]);
-	}
-  
-	Set<Integer> set = new HashSet<>();
-	int[] low = new int[numRouters + 1];
-	int[] disc = new int[numRouters + 1];
-	int parent[] = new int[numRouters + 1]; 
-	Arrays.fill(disc, -1);
-	Arrays.fill(parent, -1);
-	for(int i = 0; i <= numRouters; i++) {
-		if(disc[i] == -1 && map.containsKey(i)) {
-			dfs(map, low, disc, parent, i, set);
-    	}
-	}
-	return new ArrayList<>(set);
+	visited = new int[numRouters + 2];
+	low = new int[numRouters + 2];
+	id = 1;
+	
+	List<Integer> res = new ArrayList<>();
+	dfs(1, -1, res);
+	return res;
 }
 
-private static void dfs(Map<Integer, Set<Integer>> map, int[] low, int[] disc, int[] parent, int cur, Set<Integer> res) {
-	int children = 0; 
-	disc[cur] = low[cur]= ++time;
-	for(int nei : map.get(cur)) {
-		if(disc[nei] == -1) {
-			children++;
-			parent[nei] = cur;
-			dfs(map, low, disc, parent,nei, res);
-			low[cur] = Math.min(low[cur], low[nei]);
-			if((parent[cur] == -1 && children > 1) || (parent[cur] != -1 && low[nei] >= disc[cur]))
-				res.add(cur);
+private static void dfs(int at, int parent, List<Integer> res) {
+	visited[at] = id;
+	low[at] = id;
+	id++;
+	
+	for (int to : adj[at]) {
+		if (to == parent) {
+			continue;
 		}
-		else if(nei != parent[cur])
-			low[cur] = Math.min(low[cur], disc[nei]);
+		if (visited[to] == 0) {
+			dfs(to, at, res);
+			low[at] = Math.min(low[at], low[to]);
+			
+			if (visited[at] < low[at]) {
+				if (adj[to].size() > 1 && !res.contains(to)) {
+					res.add(to);
+				}
+				
+				if (adj[at].size() > 1 && !res.contains(at)) {
+					res.add(at);
+				}
+			}
+		} else {
+			low[at] = Math.min(low[at], visited[to]);
+		}
 	}
 }
