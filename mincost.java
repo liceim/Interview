bcb6d13826088f11a4f46832c4ddf77031b12b2a
liceim <libin.im@gmail.com>
2019-11-06 21:40:38 -0800

    Update mincost.java

--- mincost.java
+++ mincost.java
@@ -1,3 +1,14 @@
+/*
+The time to merge two files is the same as the sum of the size of the two files, which is the same as the size
+ofthe resulting merged file. We want to merge the given files into a single merged file such that every time we
+merge two files, they are the smallest available files. In order to keep track of which two files to merge next,
+we can use a priority queue (min heap) which takes out the smalles file first. Then we keep taking out the
+two smallest files, merging them, and putting that merged file back into the queue. We repeat this reduction
+until there is only a single merged file left in the queue, which gives us the time taken to merge the original
+files.
+*/
+
+//time complexity O(nlogn)
 public class Solution
 {
  	int minimumTime(int numOfSubFiles, List<Integer> files) {
