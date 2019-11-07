8ff993eb1d9a40d12c7407106b42af082e65544b
liceim <libin.im@gmail.com>
2019-11-06 21:48:48 -0800

    Update 2ListGetOptimal.java

--- 2ListGetOptimal.java
+++ 2ListGetOptimal.java
@@ -1,3 +1,10 @@
+/*
+In my thought, we can sort forwardRouteList and returnRouteList by its second element as first. 
+What we really care is the sum of route dist must be smaller than maxTravelDist and as large as possible.
+We can set two pointers and traverse two sorted arrays to find the max value (and the combination of course). 
+Since the sorting part complexity is O(MlogM + NlogN) and two-pointer traversal is O(M + N), 
+the final complexity can be regarded as O(KlogK) where K is the longest input array.
+*/
 public List<List<Integer>> optimalUtilization(int maxCapacity, List<List<Integer>> fList, List<List<Integer>> rList) {
 		Map<Integer, List<List<Integer>>> resultMap = new HashMap<>();
 		Collections.sort(fList, (f1, f2) -> (f1.get(1) - f2.get(1)));
@@ -14,7 +21,7 @@ public List<List<Integer>> optimalUtilization(int maxCapacity, List<List<Integer
 			int sumCapacity = fList.get(fIdx).get(1) + rList.get(rIdx).get(1);
 			if (sumCapacity > maxCapacity) {
 			    --rIdx;
-				  continue;
+				continue;
 			}
       
 			if (maxPossibleCapacity <= sumCapacity) {
