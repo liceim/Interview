3eb71d422beb08f777df7c72482c0361bc597287
liceim <libin.im@gmail.com>
2020-05-16 12:15:39 -0700

    Update topFrequent.java

--- topFrequent.java
+++ topFrequent.java
@@ -31,3 +31,61 @@ The word “too” has a maximum of 4 frequency but this word should be excluded
 The words "purchase” and "item" both have a maximum frequency of 4 and is not predetermined to be excluded from your analysis.
 So the output is [“item”, "purchase"] or [“purchase”, "item"], the order of words does not matter."
 */
+
+/*
+Create a set of excluded words. Split using a delimiter.
+Iterate over the map and output words with highest frequency.
+Create a map of input and frequency of each of the words, while tracking the highest frequency.
+O(n) n is length of helpString
+*/
+class Solution {
+  public static void main(String[] args) {
+      //String stri = "morning &&&& *** ^^^ %%% ------ !!!!!!!";
+      String stri = "Tanstaafl - (there's no such thing as a free lunch no't there in the lunch area) Tanstaafl free area such";
+      String[] excludes = {"a", "the", "as", "in", "there"};//area free lunch no such tanstaafl
+      
+      List<String> exclude = new ArrayList<>();
+      Collections.addAll(exclude, excludes);
+      List<String> result = getTopWords(stri, 3, exclude);
+      
+      System.out.println(result);
+  }
+  
+  public static List<String> getTopWords(String helpString, int num, List<String> excludes) {
+      Map<String, Integer> map = new HashMap<>();
+      Set<String> keySet = new HashSet<>();
+      List<String> result = new ArrayList<>();
+      if (helpString == null || helpString.length() == 0) {
+          return result;
+      }
+        
+      if (num != 0) {
+          for (int i = 0; i < excludes.size(); ++i) {
+              keySet.add(excludes.get(i).toLowerCase());
+          }
+      }
+        
+      helpString = helpString.replaceAll("\\pP"," ");
+      String[] words = helpString.split(" ");
+      int max = 0;
+      for (String word : words) {
+          word = word.toLowerCase().trim();
+          if (keySet.contains(word) || word.length() == 0) {
+              continue;
+          }
+          char c = word.charAt(0);
+          if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z')) {
+              map.put(word, map.getOrDefault(word, 0) + 1);
+              max = Math.max(max, map.get(word));
+          }
+      }
+      
+      for (String key : map.keySet()) {
+          if (map.get(key) == max) {
+              result.add(key);
+          }
+      }  
+      
+      return result;
+    }
+}
