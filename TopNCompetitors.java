34e8883c774fa644781311b7b32a329a04403887
liceim <libin.im@gmail.com>
2020-05-17 17:13:56 -0700

    Update TopNCompetitors.java

--- TopNCompetitors.java
+++ TopNCompetitors.java
@@ -2,12 +2,16 @@ import java.io.*;
 import java.util.*;
 
 /*
- * To execute Java, please define "static void main" on a class
- * named Solution.
- *
- * If you need more classes, simply define them inline.
+We can solve this problem with the similar idea:
+Firstly, we need to calculate the frequency of each word and store the result in a hashmap.
+
+Secondly, we will use bucket sort to store words. Why? Because the minimum frequency is greater than or equal to 1 and the maximum frequency is less than or equal to the length of the input string array.
+
+Thirdly, we can define a trie within each bucket to store all the words with the same frequency. With Trie, it ensures that the lower alphabetical word will be met first, saving the trouble to sort the words within the bucket.
+
+From the above analysis, we can see the time complexity is O(n).
  */
-// Use HashMap to record key and its counts, use Bucket sort all key to get top Key
+
 // Time complexity is O(n) n is review numbers.
 
 class Solution {
